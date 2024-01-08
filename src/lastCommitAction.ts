39cc804f9ada1a3d3ec4dec9962abc90edaa5b93
Jeisson Huerfano <fricred@gmail.com>
2024-01-08 17:44:44 -0500

    Refactor generateDocumentationForLastCommit function to use git log for retrieving changed files

--- src/lastCommitAction.ts
+++ src/lastCommitAction.ts
@@ -13,23 +13,33 @@ const allowedExtensions: string[] = config.allowedExtensions || [];
 export function generateDocumentationForLastCommit() {
   console.log('Generating documentation for files involved in the last commit...');
 
-  // Get the list of changed files that are part of the upcoming push
+  // Get the list of changed files in the last commit using git log
+  const lastCommitHash = shell
+    .exec('git rev-parse HEAD', { silent: true })
+    .trim();
+
   const changedFiles = shell
-    .exec('git diff --cached --name-only', { silent: true })
+    .exec(`git diff-tree --no-commit-id --name-only -r ${lastCommitHash}`, {
+      silent: true,
+    })
     .trim()
     .split('\n');
 
   // Filter files based on includedDirectories and allowedExtensions
   const filteredFiles = changedFiles.filter((file) => {
     // Check if the file is in one of the included directories
-    const isIncludedDirectory = includedDirectories.some((dir: string) => file.startsWith(dir));
+    const isIncludedDirectory = includedDirectories.some((dir: string) =>
+      file.startsWith(dir)
+    );
 
     // Check if the file has an allowed extension
-    const hasAllowedExtension = allowedExtensions.some((ext: string) => file.endsWith(ext));
+    const hasAllowedExtension = allowedExtensions.some((ext: string) =>
+      file.endsWith(ext)
+    );
 
     return isIncludedDirectory && hasAllowedExtension;
   });
-
+  console.log("🚀 ~ file: lastCommitAction.ts:53 ~ filteredFiles.forEach ~ filteredFiles:", filteredFiles)
   filteredFiles.forEach((file) => {
     // Read the file content
     const content = readFileSync(file, 'utf-8');
@@ -40,6 +50,7 @@ export function generateDocumentationForLastCommit() {
     // Write the updated content back to the file
     writeFileSync(file, updatedContent);
   });
+  
 
   console.log('Documentation generated for files involved in the last commit.');
 }
